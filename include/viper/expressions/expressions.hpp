42e725b8de8116af69c8be12a14e601041fd2e4b
Kamal <kamal@eidolonsystems.com>
2018-05-30 17:28:37 -0400

    Added support for literal value expressions.

--- include/viper/expressions/expressions.hpp
+++ include/viper/expressions/expressions.hpp
@@ -2,6 +2,7 @@
 #define VIPER_EXPRESSIONS_HPP
 #include "viper/expressions/expression.hpp"
 #include "viper/expressions/infix_operator.hpp"
+#include "viper/expressions/literal_expression.hpp"
 #include "viper/expressions/symbol.hpp"
 #include "viper/expressions/virtual_expression.hpp"
 
---
+++ include/viper/expressions/literal_expression.hpp
@@ -0,0 +1,39 @@
+#ifndef VIPER_LITERAL_EXPRESSION_HPP
+#define VIPER_LITERAL_EXPRESSION_HPP
+#include "viper/expressions/expression.hpp"
+
+namespace viper {
+
+  //! Implements an SQL expression representing a literal value.
+  class literal_expression final : public virtual_expression {
+    public:
+
+      //! Constructs a literal expression.
+      /*!
+        \param value The value to represent.
+      */
+      literal_expression(std::string value);
+
+      void append_query(std::string& query) const override;
+
+    private:
+      std::string m_value;
+  };
+
+  //! Makes a literal expression from a raw value.
+  template<typename T>
+  expression literal(const T& value) {
+    std::string v;
+    convert_to_sql(value, v);
+    return expression(std::make_shared<literal_expression>(std::move(v)));
+  }
+
+  inline literal_expression::literal_expression(std::string value)
+      : m_value(std::move(value)) {}
+
+  inline void literal_expression::append_query(std::string& query) const {
+    query += m_value;
+  }
+}
+
+#endif
