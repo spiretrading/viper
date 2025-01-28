baa7791140abd87b16d2132451812e293e71c93d
Kamal <kamal@eidolonsystems.com>
2025-01-28 14:25:58 -0500

    Updated the CMakeLists.txt to 3.28.

--- Tests/setup.bat
+++ Tests/setup.bat
@@ -1,16 +1,13 @@
 SETLOCAL
-
 pushd %~dp0
 mkdir ThirdParty
 pushd ThirdParty
-
 if exist mysql-connector-c-6.1.11-win32 goto end_mysqlconnector_setup
   wget https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-win32.zip --no-check-certificate
   if not exist mysql-connector-c-6.1.11-win32.zip goto end_mysqlconnector_setup
     unzip mysql-connector-c-6.1.11-win32.zip
     rm mysql-connector-c-6.1.11-win32.zip
 :end_mysqlconnector_setup
-
 if exist sqlite goto end_sqlite_setup
   wget --no-check-certificate https://www.sqlite.org/2020/sqlite-amalgamation-3310100.zip
   if not exist sqlite-amalgamation-3310100.zip goto end_sqlite_setup
@@ -22,12 +19,9 @@ if exist sqlite goto end_sqlite_setup
     popd
     rm sqlite-amalgamation-3310100.zip
 :end_sqlite_setup
-
 if exist Catch2 goto end_catch_setup
   git clone --branch v2.2.1 https://github.com/catchorg/Catch2.git Catch2
 :end_catch_setup
-
 popd
 popd
-
 ENDLOCAL
